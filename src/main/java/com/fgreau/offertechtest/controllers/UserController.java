2cf80ed473d44c3091cdffb0e17c35b428b7fdf1
François Gréau <francoisgreau.adressepro@gmail.com>
2023-10-06 15:28:58 +0200

    Handling logical deletion in repository

--- src/main/java/com/fgreau/offertechtest/controllers/UserController.java
+++ src/main/java/com/fgreau/offertechtest/controllers/UserController.java
@@ -4,10 +4,13 @@ import com.fgreau.offertechtest.dtos.UserDto;
 import com.fgreau.offertechtest.mappers.UserMapper;
 import com.fgreau.offertechtest.models.User;
 import com.fgreau.offertechtest.repositories.UserRepository;
+import com.fgreau.offertechtest.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +26,22 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<Object> createUser(@RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.registerUser(userDto));
+    }
+
     @GetMapping(path = "/{username}", produces = "application/json")
-    public ResponseEntity<UserDto> getUser(@PathVariable String username) {
-        final Optional<User> user = userRepository.findById(username);
+    public ResponseEntity<Object> getUser(@PathVariable String username) {
+        final Optional<User> user = userRepository.findByUsernameAndDeletedFalse(username);
         final UserDto dto = user.map(userMapper::map).orElse(null);
-        return ResponseEntity.ok(dto);
+        if (dto != null) {
+            return ResponseEntity.ok(dto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/fgreau/offertechtest/repositories/UserRepository.java
+++ src/main/java/com/fgreau/offertechtest/repositories/UserRepository.java
@@ -4,6 +4,15 @@ package com.fgreau.offertechtest.repositories;
 import com.fgreau.offertechtest.models.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, String> {
 
+    /**
+     * Get the user corresponding to its username, provided it hasn't been deleted.
+     *
+     * @param username username
+     * @return user entity
+     */
+    Optional<User> findByUsernameAndDeletedFalse(String username);
 }
